db39641be3b5321bb612153fcacd6ab9fe1a6627
Fabian.trllr <33369156+Exo-poulpe@users.noreply.github.com>
2018-12-19 15:31:34 +0100

    Add alien class with default ctor

---
+++ TrackSpaceInvaders/Alien.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackSpaceInvaders
+{
+    class Alien
+    {
+        Texture2D texture;
+        Point _position;
+        public Vector2 Speed { get; private set; }
+
+
+        public Point Position { get => _position; private set => _position = value; }
+
+        public Alien(Point position,float speed)
+        {
+            this.Position = position;
+            this.Speed = new Vector2(speed,0);
+        }
+        
+        public void LoadContent(ContentManager content)
+        {
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+        public void MoveRight()
+        {
+
+        }
+        public void MoveLeft()
+        {
+
+        }
+        public void Shoot()
+        {
+
+        }
+    }
+}
